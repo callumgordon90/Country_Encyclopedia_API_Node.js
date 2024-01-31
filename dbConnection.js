35ad08eaa376c67bdd7a567ff18014ea5b426cfc
callum <callumjosephiaingordon@gmail.com>
2024-01-31 16:14:33 +0100

    further seperation of concerns with model layer

---
+++ dbConnection.js
@@ -0,0 +1,14 @@
+// models/dbConnection.js
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '12345678',
+  database: 'laravel_countries',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
